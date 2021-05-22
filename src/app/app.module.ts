db38c8c35359d6e814c4fb5aa43a4bcacd24b8d2
Fabiano Nascimento <fabianorodrigo@gmail.com>
2021-05-22 16:40:01 -0300

    Mudança do locale para ptBR e moeda para BRL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
@@ -6,11 +6,20 @@ import { CoreModule } from './core/core.module';
 import { ErrorsModule } from './errors/errors.module';
 import { PhotosModule } from './photos/photos.module';
 import { TabelasModule } from './tabelas/tabelas.module';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
+registerLocaleData(localePt, 'pt');
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, PhotosModule, TabelasModule, ErrorsModule, CoreModule, AppRoutingModule],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt',
+    },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
